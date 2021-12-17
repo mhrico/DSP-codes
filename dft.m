2217693984133ecd7ee7446ce9088797f73c1d38
Mehedi Hassan Rico <mhrico2@gmail.com>
2021-12-17 21:05:41 +0600

    feat: plot DFT with real vs imaginary axis

--- dft.m
+++ dft.m
@@ -8,13 +8,15 @@ xn = [xn, zeros(1,N-L)];
 for k = 0: N-1
     for n = 0: N-1
         Wn = exp(-1i * 2 * pi * k * n / N);
-        X1(k + 1, n + 1) = Wn;
+        TF(k + 1, n + 1) = Wn;
     end
 end
 
-Xk = X1 * xn.';
+Xk = TF * xn.';
 k = 0:N-1;
-subplot(2,1,1);
+subplot(3,1,1);
+stem(real(Xk),imag(Xk));
+subplot(3,1,2);
 stem(k, abs(Xk));
-subplot(2,1,2);
+subplot(3,1,3);
 stem(k, angle(Xk));
